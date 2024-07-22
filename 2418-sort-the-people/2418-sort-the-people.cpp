689e0d05a5a5b3aed463621e8cd1a6a2232c275f
Arvika001 <mahimishra0601@gmail.com>
2024-07-22 12:24:26 +0530

    Time: 18 ms (96.32%), Space: 27.1 MB (46.50%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        // Create a vector of pairs (height, name)
         vector<pair<int, string>> people;
-        
-        // Populate the people vector
         for (int i = 0; i < names.size(); i++) {
             people.emplace_back(heights[i], names[i]);
         }
         
-        // Sort the people vector in descending order based on height
         sort(people.begin(), people.end(), [](const pair<int, string>& a, const pair<int, string>& b) {
             return a.first > b.first;
         });
         
-        // Extract the sorted names
         vector<string> sortedNames;
         for (const auto& person : people) {
             sortedNames.push_back(person.second);
