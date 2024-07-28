62808d5390269ce0db928c3f940d856b326e72fd
Arvika001 <mahimishra0601@gmail.com>
2024-07-28 20:36:26 +0530

    Time: 81 ms (15.71%), Space: 52.4 MB (50.09%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.js
+++ 0263-ugly-number/0263-ugly-number.js
@@ -4,18 +4,16 @@
  */
 var isUgly = function(n) {
     if (n <= 0) {
-        return false;
+        return false; // Ugly numbers are positive
     }
 
-    while (n % 2 === 0) {
-        n = Math.floor(n / 2);
-    }
-    while (n % 3 === 0) {
-        n = Math.floor(n / 3);
-    }
-    while (n % 5 === 0) {
-        n = Math.floor(n / 5);
+    // Divide n by 2, 3, and 5 until it is no longer divisible
+    for (let factor of [2, 3, 5]) {
+        while (n % factor === 0) {
+            n /= factor;
+        }
     }
 
+    // If n is reduced to 1, it is an ugly number
     return n === 1;
 };
