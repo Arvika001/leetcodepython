d92b2a145fbc565494f2130d316374d3ea129700
Arvika001 <mahimishra0601@gmail.com>
2024-07-28 20:45:46 +0530

    Time: 8 ms (90.36%), Space: 43.9 MB (46.19%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false; // Powers of three are positive integers
+        }
+        
+        while (n % 3 == 0) {
+            n /= 3; // Divide n by 3 until it is no longer divisible
+        }
+        
+        return n == 1; // If n is reduced to 1, it is a power of three
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
