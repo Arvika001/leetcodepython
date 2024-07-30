f44dc7bc3994fea1176371e040f8fdfa2898b989
Arvika001 <mahimishra0601@gmail.com>
2024-07-30 18:47:06 +0530

    Time: 37 ms (52.22%), Space: 16.6 MB (25.01%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -1,15 +1,11 @@
-/**
- * @param {number} n
- * @return {boolean}
- */
-var isPowerOfTwo = function(n) {
-    if (n < 1) {
-        return false; // Powers of two are positive integers
-    }
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n < 1:
+            return False
+        if n == 1:
+            return True
+        if n % 2 == 1:
+            return False
+        return self.isPowerOfTwo(n//2)
     
-    while (n % 2 === 0) {
-        n /= 2; // Divide n by 2 until it is no longer divisible
-    }
-    
-    return n === 1; // If n is reduced to 1, it is a power of two
-};
+        
