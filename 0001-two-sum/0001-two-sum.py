4a1e206012cedebfa0f2c189392868da20b57596
Arvika001 <mahimishra0601@gmail.com>
2024-08-22 21:38:37 +0530

    Time: 59 ms (64.82%), Space: 17.8 MB (28.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         num_map = {}
         for i, num in enumerate(nums):
             complement = target - num
@@ -12,3 +7,4 @@ class Solution(object):
                 return [num_map[complement], i]
             num_map[num] = i
         return []
+        
