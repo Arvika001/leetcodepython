7faf2403f7d3b426601c7969b1204e8db2be37af
Arvika001 <mahimishra0601@gmail.com>
2024-10-06 18:15:38 +0530

    Time: 34 ms (62.11%), Space: 16.7 MB (13.04%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
@@ -6,17 +6,16 @@ class Solution:
 
         # Ensure words1 is the longer list
         if len(words1) < len(words2):
-            words1, words2 = words2, words1
+            return self.areSentencesSimilar(sentence2, sentence1)
 
         len_words1 = len(words1)
         len_words2 = len(words2)
 
-        # Check for matching from the start
+        # Check for matching from both ends
         start_index = 0
         while start_index < len_words2 and words1[start_index] == words2[start_index]:
             start_index += 1
 
-        # Check for matching from the end
         end_index = 0
         while end_index < len_words2 and words1[len_words1 - 1 - end_index] == words2[len_words2 - 1 - end_index]:
             end_index += 1
